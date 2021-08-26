ccde121f07d5ee9eb773674af9166b5c79acdaf3
Albert wu <81510642+Albert-wuchenyu@users.noreply.github.com>
2021-08-26 15:24:18 +0800

    Update Frog.py

--- version/Frog.py
+++ version/Frog.py
@@ -7,18 +7,20 @@ from version.env.Application import Application
 
 from tkinter import *
 
+
 class Frog(object):
-    def __init__(self,x,y,egg,canvas):
-        self.brainRadius=0.0
-        self.cells=[]
-        self.eye=Zone(0,0,300)
+    def __init__(self, x, y, egg, canvas):
+        self.brainRadius = 0.0
+        self.cells = []
+        self.eye = Zone(0, 0, 300)
 
-        self.moveUp=Zone(500,50,10)
-        self.moveDown=Zone(500,100,10)
-        self.moveLeft=Zone(500,150,10)
-        self.moveRight=Zone(500,200,10)
-        self.moveRandom=Zone(500,300,10)
+        self.moveUp = Zone(500, 50, 10)
+        self.moveDown = Zone(500, 100, 10)
+        self.moveLeft = Zone(500, 150, 10)
+        self.moveRight = Zone(500, 200, 10)
+        self.moveRandom = Zone(500, 300, 10)
 
+<<<<<<< Updated upstream
         self.x=x
         self.y=y
         self.xChange=0
@@ -30,3 +32,12 @@ class Frog(object):
         self.frogImageFile=PhotoImage(Application().CLASSPATH+'frog.gif')
         self.frogImage=canvas.create_image(self.x,self.y,anchor=NW,image=self.frogImageFile)
         
+=======
+        self.x = x
+        self.y = y
+        self.egg = egg
+        self.canvas = canvas
+        self.alive = True
+        self.moveCount = 0
+        self.frogImageFile = PhotoImage()
+>>>>>>> Stashed changes
