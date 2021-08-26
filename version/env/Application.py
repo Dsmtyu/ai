a61dc2d811ce55a78218383b97bd4193441a4daa
jack20081117 <530839656@qq.com>
2021-08-26 14:14:29 +0800

    add Application

---
+++ version/env/Application.py
@@ -0,0 +1,11 @@
+# Application.py
+# Application负责项目的启动，关闭等基础服务
+
+from tkinter import *
+
+CLASSPATH='C:\\Users\\admin\\Desktop\\AI\\'
+
+class Application(object):
+    def __init__(self):
+        self.tk=Tk()
+        self.canvas=Canvas(width=520,height=550,bd=0,highlightthickness=0)
