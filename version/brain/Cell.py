7780b5fe6f098a645d64582e4a6deb3a8d2ef73e
jack20081117 <530839656@qq.com>
2021-08-26 13:27:47 +0800

    Update Cell

--- version/brain/Cell.py
+++ version/brain/Cell.py
@@ -1,11 +1,11 @@
 # Cell.py
-# Cell是细胞，属性有：细胞属于的小组编号，细胞输入区，细胞输出区
+# Cell是神经元，属性有：神经元属于的小组编号，神经元输入区，神经元输出区
 
 class Cell(object):
     def __init__(self):
         self.group=0
-        self.inputs=[] #细胞输入区
-        self.outputs=[] #细胞输出区
+        self.inputs=[] #神经元输入区
+        self.outputs=[] #神经元输出区
 
     def activate(self):
         for xy in self.inputs:
