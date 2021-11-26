345264cdf5fc16ade139e665a87c74d64c2144bf
张珈瑞 <zjr2008jack@qq.com>
2021-11-26 20:24:23 +0800

    将Egg中的方法转移到CellGroup中:给出随机数据，产生随机CellGroup

--- version/egg/CellGroup.py
+++ version/egg/CellGroup.py
@@ -4,6 +4,11 @@
 # 它的母体，而且每一次测试，一些复杂的条件反射的建立都必须从头开始训练，在项目后期，有可能每个frog生命的一半时间都花在重新建立条件反射的学习过程中。
 
 from version.egg.Zone import Zone
+from random import *
+
+def nextFloat(): return randint(1,100000)/100000
+
+def nextInt(number): return randint(1,number)
 
 class CellGroup(object):
     groupInputZone=Zone()
@@ -17,5 +22,34 @@ class CellGroup(object):
     inputQtyPerCell=0
     outputQtyPerCell=0
 
-    def __init__(self):
-        pass
+    fat=0
+    inherit=False
+
+    def __init__(self,*args):
+        if len(args)==0:
+            pass
+        if len(args)==1:
+            cellGroup=args[0]
+            if not isinstance(cellGroup,CellGroup):
+                raise TypeError
+            self.groupInputZone=cellGroup.groupInputZone
+            self.groupOutputZone=cellGroup.groupOutputZone
+            self.cellInputRadius=cellGroup.cellInputRadius
+            self.cellOutputRadius=cellGroup.cellOutputRadius
+            self.cellQty=cellGroup.cellQty
+            self.inputQtyPerCell=cellGroup.inputQtyPerCell
+            self.outputQtyPerCell=cellGroup.outputQtyPerCell
+            self.fat=cellGroup.fat
+            self.inherit=cellGroup.inherit
+        if len(args)==4:
+            brainLength,randomCellQtyPerGroup,randomInputQtyPerCell,randomOutputQtyPerCell=args
+            self.inherit=False
+            self.groupInputZone=Zone(nextFloat()*brainLength,nextFloat()*brainLength,
+                                     float(nextFloat()*brainLength*0.01))
+            self.groupOutputZone=Zone(nextFloat()*brainLength,nextFloat()*brainLength,
+                                     float(nextFloat()*brainLength*0.01))
+            self.cellQty=nextInt(randomCellQtyPerGroup)
+            self.cellInputRadius=float(nextFloat()*2+0.001)
+            self.cellOutputRadius=float(nextFloat()*2+0.001)
+            self.inputQtyPerCell=nextInt(randomInputQtyPerCell)
+            self.outputQtyPerCell=nextInt(randomOutputQtyPerCell)
