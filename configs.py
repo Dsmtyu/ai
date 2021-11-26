92662644fa7e1d3e3c2fd5e4efa49023673f3854
张珈瑞 <zjr2008jack@qq.com>
2021-11-26 20:46:44 +0800

    将重要参数存储在configs.py中

--- configs.py
+++ configs.py
@@ -1,3 +1,13 @@
-import os
-
 classpath='C:\\Users\\admin\\Desktop\\AI\\'
+
+# Speed of test
+SHOW_SPEED=1
+# Steps of one test round
+STEPS_PER_ROUND=10
+DELETE_EGGS=False#每次运行是否先删除保存的蛋
+# 屏幕的长和宽
+ENV_XSIZE=300
+ENV_YSIZE=300
+FROG_BRAIN_LENGTH=1000
+FOOD_QTY=5000#食物总量
+EGG_QTY=80#蛋总量
