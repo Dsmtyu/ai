0ec3741203165bd2720aaca3821a379ab14f67ff
张珈瑞 <zjr2008jack@qq.com>
2021-11-27 09:55:22 +0800

    增加Egg类的初始化,卵+精子=>受精卵

--- version/egg/Egg.py
+++ version/egg/Egg.py
@@ -3,31 +3,53 @@
 # 它的母体，而且每一次测试，大部分条件反射的建立都必须从头开始训练，类似于人类，无论人类社会有多聪明，婴儿始终是一张白纸，需要花大量的时间从头学习。
 # -----------------------------------------------------------------------------------------------------------------------
 from version.egg.CellGroup import CellGroup
-from version.egg.Zone import Zone
-from random import randint
-
-def nextFloat(): return randint(1,100000)/100000
-
-def nextInt(number): return randint(1,number)
+from version.egg.OrganDesc import OrganDesc
+from version.brain.Organ import Organ
+from configs import *
 
 class Egg(object):
-    def __init__(self):
-        self.CELL_GROUP_QTY=30
-        self.brainRadius=1000
-        self.cellgroups=[]
+    cellGroups=[]
+    organDescs=[]
+
+    randomCellGroupQty=30 #随机生成多少个组
+    randomCellQtyPerGroup=3 #每个组有多少个脑细胞
+    randomInputQtyPerCell=3 #每个脑细胞有多少个输入触突(神经末梢)
+    randomOutputQtyPerCell=2 #每个脑细胞有多少个输出触突(树突)
+
+    def __init__(self,*args):
+        if len(args)==0:
+            pass
+        if len(args)==2:
+            xEgg,yEgg=args
+            if not isinstance(xEgg,Egg) or not isinstance(yEgg,Egg):
+                raise TypeError
+            cellGroups=[]
+            for i in range(len(xEgg.cellGroups)):
+                oldCellGroup=xEgg.cellGroups[i]
+                newCellGroup=CellGroup(oldCellGroup)
+                newCellGroup.inherit=True
+                cellGroups.append(newCellGroup)
+            yGroup=yEgg.cellGroups[nextInt(len(yEgg.cellGroups))-1]
+            cellGroups.append(yGroup)
+            for i in range(self.randomCellGroupQty):
+                cellGroups.append(CellGroup(FROG_BRAIN_LENGTH,xEgg.randomCellQtyPerGroup,
+                                            xEgg.randomInputQtyPerCell,xEgg.randomOutputQtyPerCell))
+            self.addOrganDescs()
+
 
     def createBrandNewEgg(self):#随即制造一个新的Egg
         egg=Egg()
-        for i in range(self.CELL_GROUP_QTY):
-            cellGroup=CellGroup()
-            cellGroup.groupInputZone=Zone(nextFloat()*egg.brainRadius,nextFloat()*egg.brainRadius,
-                                          float(nextFloat()*egg.brainRadius*0.01))
-            cellGroup.groupOutputZone=Zone(nextFloat()*egg.brainRadius,nextFloat()*egg.brainRadius,
-                                          float(nextFloat()*egg.brainRadius*0.01))
-            cellGroup.cellQty=nextInt(10)
-            cellGroup.cellInputRadius=float(nextFloat()*0.001)
-            cellGroup.cellOutputRadius=float(nextFloat()*0.001)
-            cellGroup.inputQtyPerCell=nextInt(10)
-            cellGroup.outputQtyPerCell=nextInt(5)
-            egg.cellgroups.append(cellGroup)
+        for i in range(egg.randomCellGroupQty):
+            egg.cellGroups.append(CellGroup(FROG_BRAIN_LENGTH,egg.randomCellQtyPerGroup,
+                                            egg.randomInputQtyPerCell,egg.randomOutputQtyPerCell))
+        egg.addOrganDescs()
         return egg
+
+    def addOrganDescs(self):
+        self.organDescs=[]
+        self.organDescs.append(OrganDesc(Organ.HUNGRY,300,100,100))
+        self.organDescs.append(OrganDesc(Organ.UP,800,400,60))
+        self.organDescs.append(OrganDesc(Organ.DOWN,800,100,60))
+        self.organDescs.append(OrganDesc(Organ.LEFT,700,250,60))
+        self.organDescs.append(OrganDesc(Organ.RIGHT,900,250,60))
+        self.organDescs.append(OrganDesc(Organ.EAT,0,0,0))
