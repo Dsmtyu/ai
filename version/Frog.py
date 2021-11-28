99eba45a599162e8ce23a6082d465b5c716a2603
张珈瑞 <zjr2008jack@qq.com>
2021-11-28 11:16:36 +0800

    将frog中的运动，找食能力转移到Organ中

--- version/Frog.py
+++ version/Frog.py
@@ -4,6 +4,7 @@
 
 from version.brain.Cell import Cell
 from version.brain.IO import Input,Output
+from version.brain.Organ import Organ
 from version.egg.Zone import Zone
 from configs import *
 
@@ -69,80 +70,26 @@ class Frog(object):
                     c.outputs.append(output)
                 self.cells.append(c)
 
+        if egg.organDescs is not None:
+            for organdesc in egg.organDescs:
+                self.organs.append(Organ(organdesc))
+
     def randomPosInZone(self,z):#在Zone区域中的随机点，即坐标在Zone内，半径为0的一个Zone
         return Zone(z.x-z.radius+z.radius*2*nextFloat(),z.y-z.radius+z.radius*2*nextFloat(),0)
 
     def active(self,env):#青蛙是否存活
+        self.energy-=20
         if not self.alive:#青蛙已死亡，返回False
             return False
         if self.x<0 or self.x>=env.ENV_XSIZE\
-        or self.y<0 or self.y>=env.ENV_YSIZE:#青蛙的横纵坐标是否出界
+        or self.y<0 or self.y>=env.ENV_YSIZE\
+        or self.energy<0:#青蛙的横纵坐标是否出界;青蛙的能量是否<0
             self.alive=False#出界时青蛙死亡
             return False
 
-        #移动青蛙
-        for cell in self.cells:
-            for output in cell.outputs:
-                if self.moveUp.nearby(output):self._moveUp(env)
-                if self.moveDown.nearby(output):self._moveDown(env)
-                if self.moveLeft.nearby(output):self._moveLeft(env)
-                if self.moveRight.nearby(output):self._moveRight(env)
-                if self.moveRandom.nearby(output):self._moveRandom(env)
-        return True
-
-    def checkFoodAndEat(self,env):#如果Frog坐标与Food坐标重合，吃掉它
-        eatedFood=False#是否吃掉食物
-        if self.x>=0 and self.x<env.ENV_XSIZE\
-        and self.y>=0 and self.y<env.ENV_YSIZE:
-            if env.foods[round(self.x)][round(self.y)]==1:
-                env.foods[round(self.x)][round(self.y)]=0
-                self.energy+=1000#吃到食物青蛙能量增加1000
-                eatedFood=True
-        if eatedFood: #TODO: 奖励措施未完成
-            pass
-
-    def _moveUp(self,env):
-        self.yChange-=self.change
-        self.y-=self.change
-        self.moveCount+=1
-        if self.y<0 or self.y>=env.ENV_YSIZE:
-            self.alive=False
-            return None
-        self.checkFoodAndEat(env)
-
-    def _moveDown(self,env):
-        self.yChange+=self.change
-        self.y+=self.change
-        self.moveCount+=1
-        if self.y<0 or self.y>=env.ENV_YSIZE:
-            self.alive=False
-            return None
-        self.checkFoodAndEat(env)
-
-    def _moveLeft(self,env):
-        self.xChange-=self.change
-        self.x-=self.change
-        self.moveCount+=1
-        if self.x<0 or self.x>=env.ENV_XSIZE:
-            self.alive=False
-            return None
-        self.checkFoodAndEat(env)
-
-    def _moveRight(self,env):
-        self.xChange+=self.change
-        self.x+=self.change
-        self.moveCount+=1
-        if self.x<0 or self.x>=env.ENV_XSIZE:
-            self.alive=False
-            return None
-        self.checkFoodAndEat(env)
-
-    def _moveRandom(self,env):
-        rand=nextInt(4)
-        if rand==1:self._moveUp(env)
-        if rand==2:self._moveDown(env)
-        if rand==3:self._moveLeft(env)
-        if rand==4:self._moveRight(env)
+        for organ in self.organs:
+            organ.active(self)
+        return self.alive
 
     def show(self,canvas):
         if not self.alive:
