d4dbc86b274d70d6285fe035f9aa37513653f2a1
张珈瑞 <zjr2008jack@qq.com>
2022-01-01 12:14:35 +0000

    删除文件 version

--- version/egg/OrganDesc.py
+++
@@ -1,8 +0,0 @@
-from version.egg.Zone import Zone
-
-class OrganDesc(Zone):
-    type=0
-
-    def __init__(self,type,x,y,radius):
-        super(OrganDesc,self).__init__(x,y,radius)
-        self.type=type
